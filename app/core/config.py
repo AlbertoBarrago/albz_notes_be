c719656c2668e80be32b42440732be99ab943392
Alberto Barrago <albertobarrago@gmail.com>
2024-11-15 16:20:12 +0100

    Update

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,7 @@
-"""
-Config props
-"""
-import os
-
 from dotenv import load_dotenv
-
 from pydantic.v1 import BaseSettings
 
 load_dotenv()
-
 
 class Settings(BaseSettings):
     """
@@ -18,15 +11,13 @@ class Settings(BaseSettings):
     MYSQL_PASSWORD: str
     MYSQL_HOST: str
     MYSQL_DATABASE: str
-    SECRET_KEY: str = (
-        os
-        .getenv("SECRET_KEY", "8d0f39701a43810766d0c9fa25acd6f0097dff05c2d0322d8983969c88c81bd8"))
+    SECRET_KEY: str = "8d0f39701a43810766d0c9fa25acd6f0097dff05c2d0322d8983969c88c81bd8"  # Default value
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_SECRET_KEY: str = os.getenv("GOOGLE_SECRET_KEY")
-    RATE_LIMIT: str = os.getenv("RATE_LIMIT")
-    RATE_LIMIT_WINDOW: str = os.getenv("RATE_LIMIT_WINDOW")
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_SECRET_KEY: str
+    RATE_LIMIT: int  # Cambiato in int
+    RATE_LIMIT_WINDOW: int  # Cambiato in int
 
     class Config:
         """
