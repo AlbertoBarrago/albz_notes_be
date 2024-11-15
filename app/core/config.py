fe44ea85bf61a6a6595a3478e0aaec2852afa5d1
Alberto Barrago <albertobarrago@gmail.com>
2024-11-15 16:37:08 +0100

    Updated settins with env value

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,8 @@
 """
     Config file
 """
+import os
+
 from dotenv import load_dotenv
 from pydantic.v1 import BaseSettings
 
@@ -10,22 +12,17 @@ class Settings(BaseSettings):
     """
     Settings class
     """
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
-    MYSQL_HOST: str
-    MYSQL_DATABASE: str
-    SECRET_KEY: str = "8d0f39701a43810766d0c9fa25acd6f0097dff05c2d0322d8983969c88c81bd8"  # Default value
+    MYSQL_USER: str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST: str = os.getenv("MYSQL_HOST")
+    MYSQL_DATABASE: str = os.getenv("MYSQL_DATABASE")
+    SECRET_KEY: str = "8d0f39701a43810766d0c9fa25acd6f0097dff05c2d0322d8983969c88c81bd8"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    GOOGLE_CLIENT_ID: str
-    GOOGLE_SECRET_KEY: str
-    RATE_LIMIT: int
-    RATE_LIMIT_WINDOW: int
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_SECRET_KEY: str = os.getenv("GOOGLE_SECRET_KEY")
+    RATE_LIMIT: int = 1000
+    RATE_LIMIT_WINDOW: int = 60
 
-    class Config:
-        """
-        Config class
-        """
-        env_file = "../../.env"
 
 settings = Settings()
