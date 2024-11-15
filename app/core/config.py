c8489507a49bfc1f74bedb79b3d37616a085507e
Alberto Barrago <albertobarrago@gmail.com>
2024-11-15 16:50:20 +0100

    Try this config

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     """
     MYSQL_USER: str = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD")
-    MYSQL_HOST: str = os.getenv("MYSQL_HOST")
+    MYSQL_HOST: str = os.getenv("MYSQL_HOST", "localhost:3306")
     MYSQL_DATABASE: str = os.getenv("MYSQL_DATABASE")
     SECRET_KEY: str = "8d0f39701a43810766d0c9fa25acd6f0097dff05c2d0322d8983969c88c81bd8"
     ALGORITHM: str = "HS256"
