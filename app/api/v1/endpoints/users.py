5ffd0f4795db49f5ad8b8a91b7ae292c54c11a3d
Alberto Barrago <albertobarrago@gmail.com>
2024-11-21 11:49:49 +0100

    Improved User Service
    - Added UserManager

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -10,7 +10,7 @@ from app.db.models import User
 from app.db.mysql import get_db, get_current_user
 from app.schemas.login import TokenResponse
 from app.schemas.user import UserOut, UserRequestAdd, PasswordReset, GoogleResetRequest
-from app.utils.user.actions import perform_action_user
+from app.utils.user.actions import UserManager
 
 router = APIRouter()
 
@@ -23,12 +23,12 @@ async def register_user(user: UserRequestAdd, db: Session = Depends(get_db)):
     :param db:
     :return: UserOut
     """
-    return await perform_action_user(db, "register_user", user=user)
+    return await UserManager(db).perform_action_user("register_user", user=user)
 
 
 @router.post("/reset/password")
 async def reset_password(password_reset: PasswordReset,
-                   db: Session = Depends(get_db)):
+                         db: Session = Depends(get_db)):
     """
     Reset user password
     :param password_reset:
@@ -36,16 +36,16 @@ async def reset_password(password_reset: PasswordReset,
     :return: Success message
     """
 
-    return await perform_action_user(db,
-                               "reset_password",
-                               user_username=password_reset.username,
-                               new_password=password_reset.new_password,
-                               current_password=password_reset.current_password)
+    return await (UserManager(db)
+                  .perform_action_user("reset_password",
+                                       user_username=password_reset.username,
+                                       new_password=password_reset.new_password,
+                                       current_password=password_reset.current_password))
 
 
 @router.post("/reset/google-password")
 async def reset_google_password(google_req: GoogleResetRequest,
-                          db: Session = Depends(get_db)):
+                                db: Session = Depends(get_db)):
     """
     Reset user password
     :param google_req:
@@ -57,24 +57,24 @@ async def reset_google_password(google_req: GoogleResetRequest,
     if not payload:
         raise HTTPException(status_code=400, detail="Invalid or expired token")
 
-    return await perform_action_user(db,
-                               "reset_google_password",
-                               user_username=payload,
-                               new_password=google_req.new_password)
+    return await UserManager(db).perform_action_user(
+        "reset_google_password",
+        user_username=payload,
+        new_password=google_req.new_password)
 
 
 @router.get("/me", response_model=UserOut)
 async def get_current_user_info(current_user: User = Depends(get_current_user),
-                          db: Session = Depends(get_db)):
+                                db: Session = Depends(get_db)):
     """
     Get current user information
     :param current_user:
     :param db:
     :return: UserOut
     """
-    return await perform_action_user(db,
-                               "me",
-                               current_user=current_user)
+    return await UserManager(db).perform_action_user(
+        "me",
+        current_user=current_user)
 
 
 @router.put("/update", response_model=UserOut)
@@ -88,20 +88,19 @@ async def update_user(user_update: UserRequestAdd,
     :param current_user:
     :return: UserOut
     """
-    return await perform_action_user(db, "update_user",
-                               user=user_update,
-                               current_user=current_user)
+    return await UserManager(db).perform_action_user("update_user",
+                                                     user=user_update,
+                                                     current_user=current_user)
 
 
 @router.delete("/delete")
 async def delete_user(db: Session = Depends(get_db),
-                current_user: User = Depends(get_current_user)):
+                      current_user: User = Depends(get_current_user)):
     """
     Delete user account
     :param db:
     :param current_user:
     :return: Success message
     """
-    return await perform_action_user(db,
-                               "delete_user",
-                               current_user=current_user)
+    return await UserManager(db).perform_action_user("delete_user",
+                                                     current_user=current_user)
